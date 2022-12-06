1aebd26d938798868cd53924897a2c0d7b15900b
itsmeabhishekn <abhishekn4454@gmail.com>
2022-12-06 17:22:05 +0530

    added view pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseAddComponent } from './course-add/course-add.component';
+import { CourseViewComponent } from './course-view/course-view.component';
+
+const myRoute:Routes = [
+  {path:"",component:CourseAddComponent},
+  {path:"add",component:CourseAddComponent},
+  {path:"view",component:CourseViewComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseAddComponent
+    CourseAddComponent,
+    CourseViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
